e637d1350e9f9cecbb9e0d04303511f032ae9f4a
Madusha Akalanka <madushaakalanka97@gmail.com>
2023-08-20 21:57:00 +0530

    Complete Create New ACC

--- SmartBanking.java
+++ SmartBanking.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SmartBanking {
@@ -9,6 +10,7 @@ public class SmartBanking {
     final static String RESET = "\033[0m";
     final static String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
     final static String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
+    static String[][] bankAccount = new String[0][];
 
     public static void main(String[] args) {
         final String CLEAR = "\033[H\033[2J";
@@ -78,51 +80,24 @@ public class SmartBanking {
                     break;
                 // Create New Account
                 case NEW_ACC:
-                    System.out.printf("\tNew Student ID: SDB-%05d \n", (account.length + 1));
-                    String id = String.format("SDB-%05d", account.length + 1);
-
-                    boolean valid;
                     String name;
+                    double deposit = 0.0;
+                    System.out.printf("\tNew Student ID: SDB-%05d \n", (account.length + 1));
+                    String id = String.format("SDB-%05d", bankAccount.length + 1);
+
                     name = nameValidation();
-
-                    double deposit;
-                    do {
-                        valid = true;
-                        System.out.print("\tEnter Initial Deposit: ");
-                        deposit = SCANNER.nextDouble();
-                        SCANNER.nextLine();
-
-                        if (deposit < 5000) {
-                            System.out.printf("\t%sInitial deposit should be more than 5000%s\n", COLOR_RED_BOLD,
-                                    RESET);
-                            valid = false;
-                            continue;
-
-                        }
-
-                    } while (!valid);
-
-                    String[][] tempAccounts = new String[account.length + 1][3];
-                    String depositString = String.valueOf(deposit);
-                    for (int i = 0; i < account.length; i++) {
-                        tempAccounts[i] = account[i];
-
-                    }
-
-                    tempAccounts[tempAccounts.length - 1][0] = id;
-                    tempAccounts[tempAccounts.length - 1][1] = name;
-                    tempAccounts[tempAccounts.length - 1][2] = depositString;
-
-                    account = tempAccounts;
-
+                    deposit = depositValidation();
+                    valueInsertToArray(id, name, deposit);
+               
                     System.out.println();
                     System.out.printf(SUCCESS_MSG,
-                            String.format("%s:%s has been saved successfully", id, name));
+                    String.format("%s:%s has been saved successfully", id, name));
                     System.out.print("\tDo you want to continue adding (Y/n)? ");
                     if (SCANNER.nextLine().strip().toUpperCase().equals("Y"))
                         continue;
                     screen = DASHBOARD;
                     break;
+                    
 
                 case DEPOSIT_MONEY:
 
@@ -131,6 +106,7 @@ public class SmartBanking {
         } while (true);
     }
 
+    // Name Validation Method
     public static String nameValidation() {
         String name;
         boolean valid = true;
@@ -152,11 +128,47 @@ public class SmartBanking {
                 }
             }
         } while (!valid);
-
         return name;
+    }
+
+    // Deposit Validation
+    public static double depositValidation() {
+        boolean valid;
+        double deposit;
+        do {
+            valid = true;
+            System.out.print("\tEnter Initial Deposit: ");
+            deposit = SCANNER.nextDouble();
+            SCANNER.nextLine();
+
+            if (deposit < 5000) {
+                System.out.printf("\t%sInitial deposit should be more than 5000%s\n", COLOR_RED_BOLD,
+                        RESET);
+                valid = false;
+                continue;
+
+            }
+
+        } while (!valid);
+        return deposit;
 
     }
 
-    
+    // Insert Data into BankAccount Array
+
+    public static void valueInsertToArray(String id, String name, double deposit) {
+        String[][] tempAccounts = new String[bankAccount.length + 1][3];
+        String depositString = String.valueOf(deposit);
+        for (int i = 0; i < bankAccount.length; i++) {
+            tempAccounts[i] = bankAccount[i];
+
+        }
+
+        tempAccounts[tempAccounts.length - 1][0] = id;
+        tempAccounts[tempAccounts.length - 1][1] = name;
+        tempAccounts[tempAccounts.length - 1][2] = depositString;
+
+        bankAccount = tempAccounts;
+    }
 
 }
