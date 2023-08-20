f6ff4cefbd55189e6fc05bb30e47ba31eeea8699
Madusha Akalanka <madushaakalanka97@gmail.com>
2023-08-20 21:32:24 +0530

    Name Validation

--- SmartBanking.java
+++ SmartBanking.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class SmartBanking {
     private static final Scanner SCANNER = new Scanner(System.in);
 
+    final static String COLOR_BLUE_BOLD = "\033[34;1m";
+    final static String COLOR_RED_BOLD = "\033[31;1m";
+    final static String COLOR_GREEN_BOLD = "\033[33;1m";
+    final static String RESET = "\033[0m";
+    final static String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
+    final static String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
+
     public static void main(String[] args) {
         final String CLEAR = "\033[H\033[2J";
-        final String COLOR_BLUE_BOLD = "\033[34;1m";
-        final String COLOR_RED_BOLD = "\033[31;1m";
-        final String COLOR_GREEN_BOLD = "\033[33;1m";
-        final String RESET = "\033[0m";
-
         final String DASHBOARD = "\u2795 Welcome to Smart Banking System";
         final String NEW_ACC = "\u2795  Open New Account";
         final String DEPOSIT_MONEY = "\u2795 Deposit Money";
@@ -17,9 +19,6 @@ public class SmartBanking {
         final String TRANSFER = "\u1F91D Transfer Money";
         final String CHECK_BALANCE = "🖨 Check Balance";
         final String DELETE_AC = "\u274C Delete Account";
-
-        final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
-        final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
 
         // Arrays
 
@@ -75,6 +74,7 @@ public class SmartBanking {
                             continue;
 
                     }
+
                     break;
                 // Create New Account
                 case NEW_ACC:
@@ -83,24 +83,7 @@ public class SmartBanking {
 
                     boolean valid;
                     String name;
-                    do {
-                        valid = true;
-                        System.out.print("\tName: ");
-                        name = SCANNER.nextLine().strip();
-                        if (name.isBlank()) {
-                            System.out.printf("\t%sName can't be empty%s\n", COLOR_RED_BOLD, RESET);
-                            valid = false;
-                            continue;
-                        }
-                        for (int i = 0; i < name.length(); i++) {
-                            if (!(Character.isLetter(name.charAt(i)) ||
-                                    Character.isSpaceChar(name.charAt(i)))) {
-                                System.out.printf("\t%sInvalid Name%s\n", COLOR_RED_BOLD, RESET);
-                                valid = false;
-                                break;
-                            }
-                        }
-                    } while (!valid);
+                    name = nameValidation();
 
                     double deposit;
                     do {
@@ -113,6 +96,7 @@ public class SmartBanking {
                             System.out.printf("\t%sInitial deposit should be more than 5000%s\n", COLOR_RED_BOLD,
                                     RESET);
                             valid = false;
+                            continue;
 
                         }
 
@@ -140,9 +124,39 @@ public class SmartBanking {
                     screen = DASHBOARD;
                     break;
 
+                case DEPOSIT_MONEY:
+
             }
 
         } while (true);
     }
+
+    public static String nameValidation() {
+        String name;
+        boolean valid = true;
+        do {
+            System.out.print("\tName: ");
+            name = SCANNER.nextLine().strip();
+            valid = true;
+            if (name.isBlank()) {
+                System.out.printf("\t%sName can't be empty%s\n", COLOR_RED_BOLD, RESET);
+                valid = false;
+                continue;
+            }
+            for (int i = 0; i < name.length(); i++) {
+                if (!(Character.isLetter(name.charAt(i)) ||
+                        Character.isSpaceChar(name.charAt(i)))) {
+                    System.out.printf("\t%sInvalid Name%s\n", COLOR_RED_BOLD, RESET);
+                    valid = false;
+                    break;
+                }
+            }
+        } while (!valid);
+
+        return name;
+
+    }
+
+    
 
 }
